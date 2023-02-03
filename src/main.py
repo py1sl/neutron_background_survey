576767e7a1855ab233f4eaf607027fdacf8bf5cd
e-reyno <elliot.reynolds@stfc.ac.uk>
2023-02-03 13:51:59 +0000

    remove prints

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ def main(selected_building):
     "selected building : 'any', 'ts1', 'ts2'"
     shutters = da.load_pickle(fname)
     diamon_path = da.select_location(selected_building)
-    print(diamon_path)
     data = dia.read_diamon_folders(diamon_path, location_path)
     beam_data = {name: da.filter_shutters(dic, shutters) for name, dic in data.items()}
     beam_df = da.convert_to_df(beam_data)
