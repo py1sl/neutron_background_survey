e79cdf005fa3c244381d379f684471b14e6d8278
e-reyno <elliot.reynolds@stfc.ac.uk>
2023-02-03 14:08:04 +0000

    Revert "read changes"
    This reverts commit de1b24ae016269d40082297565d93c75d35b9be6.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 import diamon_read_data as dia
 import diamon_analysis as da
 
-location_path = "data\Measurement_location.csv"
+ts2_path = r"C:\Users\sfs81547\OneDrive - Science and Technology Facilities Council\Documents\ISIS\Diamon Project\TS2 Measurements\DIAMON*"
+location_path = r"C:\Users\sfs81547\OneDrive - Science and Technology Facilities Council\Documents\ISIS\Diamon Project\Code\data\Measurement_location.csv"
 fname = "shutters_pickle.pkl"
 
-def main(selected_building):
-    "selected building : 'any', 'ts1', 'ts2'"
+def main():
     shutters = da.load_pickle(fname)
-    diamon_path = da.select_location(selected_building)
-    print(diamon_path)
-    data = dia.read_diamon_folders(diamon_path, location_path)
+    data = dia.read_diamon_folders(ts2_path, location_path)
     beam_data = {name: da.filter_shutters(dic, shutters) for name, dic in data.items()}
     beam_df = da.convert_to_df(beam_data)
     return beam_data, beam_df
 
 if __name__ == "__main__":
-    main("all")
+    main()
