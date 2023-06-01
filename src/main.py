95a61432b011571ddd2194e53c4d47027510b205
e-reyno <elliot.reynolds@stfc.ac.uk>
2023-06-01 08:19:46 +0100

    new read data

--- src/main.py
+++ src/main.py
@@ -1,20 +1,29 @@
-import diamon_read_data as dia
-import diamon_analysis as da
+import src.diamon as d
+import src.diamon_analysis as da
+import src.shutter_analysis as sa
 
-ts2_path = r"C:\Users\sfs81547\Documents\diamon\ts2\DIAMON*"
-location_path = r"C:\Users\sfs81547\OneDrive - Science and Technology Facilities Council\Documents\ISIS\Diamon Project\Code\data\Measurement_location.csv"
-fname = "shutters_pickle.pkl"
+def main(save=False):
+    """_summary_
 
-def main():
-    shutters = da.load_pickle(fname)
-    data = dia.read_diamon_folders(ts2_path, location_path)
-    beam_data = {name: da.filter_shutters(dic, shutters) for name, dic in data.items()}
-    beam_df = da.convert_to_df(beam_data)
-    open_shutters = da.filter_shutter_status(data, True)
-    closed_shutters = da.filter_shutter_status(data, False)
+    Returns:
+        _type_: _description_
+    """
+    shutter_data = sa.load_shutter_data()
+    main_data = d.read_data(shutter_data)
+    main_data = {key: sa.filter_shutters(result, shutter_data) for key, result in main_data.items()}
+    # save
+    if save == True:
+        da.save_pickle(main_data, "diamon_data")
+    return main_data
 
-    return {"all_data": beam_data, "df":beam_df, "open_shutter": open_shutters, 
-            "closed_shutter": closed_shutters}
+def shutter_df(data, selected_shutter, bb):
+    "return panda df showing selected shutter status for data"
+    df = da.filter_shutter_status(data, selected_shutter, bb)
+    open_shutters = da.flag_shutter(df, selected_shutter, True)
+    closed_shutters = da.flag_shutter(df, selected_shutter, False)
+    open_shutters = da.find_abs_error(open_shutters, "norm_dose", "Hr_un%")
+    closed_shutters = da.find_abs_error(closed_shutters, "norm_dose", "Hr_un%")
+    return {"data":data, "df":df, "open-shutter":open_shutters, "closed-shutter":closed_shutters}
 
 if __name__ == "__main__":
     main()
