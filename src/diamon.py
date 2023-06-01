d5e125cbdeb783b3477b606730e9c97e28a2c91e
e-reyno <elliot.reynolds@stfc.ac.uk>
2023-06-01 08:12:56 +0100

    filter_current

--- src/diamon.py
+++ src/diamon.py
@@ -1,16 +1,19 @@
-import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 import glob
+from pathlib import Path
+import re
 import pytz
 import src.diamon_analysis as da
 import src.neutronics_analysis as na
 import src.shutter_analysis as sa
-import re
-import os.path
+
+loc_path = "data\Measurement_location.csv"
+diamon_path = "data\measurements\DIAMON*"
 
 class diamon:
-    def __init__(self, summary_out_selected: bool):
+    def __init__(self, folder_path="", summary_out_selected=False):
+        
         self.out_data : pd.DataFrame()
         self.rate_data : pd.DataFrame()
         self.datetime : datetime
@@ -22,7 +25,6 @@ class diamon:
         self.dose : float
         self.unfold_data = pd.Series(dtype=object)
         self.shutters = []
-        self.beamlines = []
         self.energy_type = "low"
         self.time = None
         self.start_time = None
@@ -30,6 +32,9 @@ class diamon:
         self.id = None
         self.summary = summary_out_selected
         self.current_data = []
+        if folder_path != "":
+            self.read_folder(folder_path)
+            self.folder_name = folder_path.split("\\")[-1]
 
     def __str__(self):
         return self.file_name
@@ -43,7 +48,7 @@ class diamon:
                 elif "GMT time" in lines[i-1]:
                     start_time = datetime.strptime(line, "%a %b %d %H:%M:%S %Y")
                     self.start_time = pytz.timezone("Europe/London").localize(start_time)
-                    self.end_time = start_time + timedelta(seconds=float(self.time))
+                    self.end_time = self.start_time + timedelta(seconds=float(self.time))
                 elif "Serial" in line:
                     serial = re.findall(r"\d$", line)[0]
         f.close()
@@ -57,37 +62,38 @@ class diamon:
         Returns:
             _type_: _description_
         """
+        self.unfold_data  ={}
         spect_data = False
         with open(path) as f:
             for line in f:
                 if " thermal" in line:
-                    self.unfold_data["thermal"] = clean_param(line)
+                    self.unfold_data["thermal"] = na.clean_param(line)
                 elif "epi" in line:
-                    self.unfold_data["epi"] = clean_param(line)
+                    self.unfold_data["epi"] = na.clean_param(line)
                 elif "fast" in line:
-                    self.unfold_data["fast"] = clean_param(line)
+                    self.unfold_data["fast"] = na.clean_param(line)
                 elif "phi" in line:
-                    self.unfold_data["phi"], self.unfold_data["phi_uncert"] = clean_param(line, True)
+                    self.unfold_data["phi"], self.unfold_data["phi_uncert"] = na.clean_param(line, True)
                 elif "H*(10)_r" in line:
-                    self.unfold_data["dose_rate"],  self.unfold_data["dose_rate_uncert"] = clean_param(line, True)
+                    self.unfold_data["dose_rate"],  self.unfold_data["dose_rate_uncert"] = na.clean_param(line, True)
                 elif "h*(10)" in line:
-                    self.unfold_data["dose_area_product"], self.unfold_data["dose_area_product_uncert"] = clean_param(line, True)
+                    self.unfold_data["dose_area_product"], self.unfold_data["dose_area_product_uncert"] = na.clean_param(line, True)
                 elif "D1" in line:
-                    self.unfold_data["count_D1"], self.unfold_data["count_R"] = clean_counts(line)
+                    self.unfold_data["count_D1"], self.unfold_data["count_R"] = na.clean_counts(line)
                 elif "D2" in line:
-                    self.unfold_data["count_D2"], self.unfold_data["count_RL"] = clean_counts(line)
+                    self.unfold_data["count_D2"], self.unfold_data["count_RL"] = na.clean_counts(line)
                 elif "D3" in line:
-                    self.unfold_data["count_D3"], self.unfold_data["count_FL"] = clean_counts(line)
+                    self.unfold_data["count_D3"], self.unfold_data["count_FL"] = na.clean_counts(line)
                 elif "D4" in line:
-                    self.unfold_data["count_D4"], self.unfold_data["count_F"] = clean_counts(line)
+                    self.unfold_data["count_D4"], self.unfold_data["count_F"] = na.clean_counts(line)
                 elif "D5" in line:
-                    self.unfold_data["count_D5"], self.unfold_data["count_FR"] = clean_counts(line)
+                    self.unfold_data["count_D5"], self.unfold_data["count_FR"] = na.clean_counts(line)
                 elif "D6" in line:
-                    self.unfold_data["count_D6"], self.unfold_data["count_RR"] = clean_counts(line)
+                    self.unfold_data["count_D6"], self.unfold_data["count_RR"] = na.clean_counts(line)
                 elif "TIME" in line:
-                    self.unfold_data["time"] = float(clean_param(line))
+                    self.unfold_data["time"] = float(na.clean_param(line))
                 elif spect_data and ("----" not in line):
-                    line = clean(line)
+                    line = na.clean(line)
                     if len(line) < 1:
                         break
                     self.energy_bin.append(float(line[0]))
@@ -131,85 +137,8 @@ class diamon:
                 print("Error: please input a valid folder directory")
                 break
 
-    def get_shutter_info(self, shutter):
-        beamlines = da.get_east_west_names(self)
-    def filter_current(self, current_df, times, i):
-        time = times[i]
-        #first measurement take last data 20s before
-        if i ==0:
-            start = times[i] - np.timedelta64(20, 's')
-        else:
-            #take start as value before
-            start = times[i-1]
-        filtered_current = current_df.loc[pd.IndexSlice[:, start:time], :]["_value"]
-        return np.mean(filtered_current)
-    def shutter_filter(self, shutter_df, current_df):
-        values = []
-        current_values = []
-        times = np.array(self.out_data["datetime"])
-        for i, time in enumerate(times):
-            shutter_df 
-            values.append(shutter_df.loc[pd.IndexSlice[:, :time], :].groupby(shutter_df.index.names[0]).tail(1).droplevel("datetime"))
-            current_values.append(self.filter_current(current_df, times, i))
-        return values
-
-def load_diamon(loc_data, fname, shutter_data):
-    """data from diamon instrument loaded from files. data matched with measurement coordinate
-
-    Args:
-        loc_data (df): pandas dataframe of x, y, z coordinates and data reference
-        fname (str): folder directory to diamon data
-
-    Returns:
-        dictionary of diamon class objects
-    """
-    diamon_list = []
-    series_list = []
-    for folder in glob.glob(fname):
-        diamon_obj = diamon(False)
-        diamon_obj.read_folder(folder)
-        diamon_obj.folder_name = os.path.basename(folder)
-        #print(diamon_obj.out_data)
-        diamon_list.append(diamon_obj)
-        series_list.append(diamon_obj.id)
-    # match the location from the file name ref to coord to get shutter info
-    # add option to read in shutter info relevent to obj
-    id = da.get_measurement_id(pd.DataFrame(series_list))
-    shutter_df = pd.concat(shutter_data["shutters"].values(), keys=shutter_data["shutters"].keys(), names=["beamline", "datetime"])
-    current_df = pd.concat(shutter_data["current"].values(), keys=shutter_data["current"].keys(),  names=["target_station", "datetime"])
-    for data in diamon_list:
-        data = match_location(loc_data, data, id)
-        data.beamlines = da.get_east_west_names(data.reference)
-        data.beamline = da.get_names(data.reference["Measurement Reference"].iloc[0])[1]
-        #data.filter_shutters(shutter_df, current_df)
-        #shutter_info = [sa.filter_shutters([shutter_df, current_df], data.start_time, time, [shutter_df.index.names[0], current_df.index.names[0]]) for time in data.out_data["datetime"]]
-    return diamon_list
-
-def split_influx_data(influx_dic, splitby="current"):
-    """Split shutter and current information into 2 keys of a nested dic
-
-    Args:
-        influx_dic (_type_): _description_
-    """
-    if splitby == "current":
-        keys = ["ts1_current", "ts2_current"]
-        
-
-def match_location(location_data, data, id):
-    """match location csv file key to data file name
-
-    Args:
-        location_data (df): pd df of xyz and ref.
-        diamon_list (list): list of diamon class obj
-        series_list (list): list of pd data series containing file id's
-
-    Returns:
-        dic: list of diamon obj with keys assigned
-    """
-    # load location
-    data.file_name = id[id["start"] == data.start_time]["key"].values[0]
-    data.reference = location_data.loc[location_data["Name"] == data.file_name].reset_index()
-    return data
+    def get_shutter_name(self):
+        return self.beamlines.name
 
 def read_data_file(path, i, j):
     """_summary_
@@ -231,54 +160,106 @@ def read_data_file(path, i, j):
             data[col]= data[col].astype(float)
     return data
 
-def get_measurement_id(df):
-    """generates unique id based on serial and which repeat of the day
+# read all data
+def read_data(shutter_data):
+    # try to load exisiting data
+
+    data = da.load_pickle("diamon_data")
+    loc_data = na.read_csv(loc_path, {'x': float, 'y': float, 'z':float})
+    if data:
+        print("checking if there is new data in the directory")
+        data = update_diamon_data(data, loc_data, shutter_data)
+    else:
+        print("no data - loading all data in file path")
+        data = load_diamon(loc_data, shutter_data)
+    return data
+
+def update_diamon_data(existing_data, loc_data, shutter_data):
+    """_summary_
 
     Args:
-        df (_type_): _description_
+        existing_data (_type_): _description_
+        loc_data (_type_): _description_
+        shutter_data (_type_): _description_
 
     Returns:
         _type_: _description_
     """
-    sorted_df = df.sort_values(by="start").reset_index()
+    # check if missing
+    latest_data = get_recent_entry(existing_data)
+    file_paths = glob.glob(r"data\ts2_measurements\DIAMON*\counters.txt")
+    unread_paths = [x for file_path in file_paths if (x := read_unloaded_file(file_path, latest_data)) is not None]
+    if len(unread_paths) == 0:
+        print("No new data - loading existing file")
+        return existing_data
+    else:
+         new_dic = load_diamon(loc_data, unread_paths, shutter_data)
+         existing_data.update(new_dic)
+         # SAVE NEW DATA
+         na.save_pickle(existing_data, "main_data")
+         return new_dic
+
+def get_recent_entry(data):
+    # in exisitng group fo diamon results get most recent entry
+    return max([file.start_time for file in data.values()])
+
+def read_unloaded_file(file_path, latest):
+    match_string = "GMT time"
+    # record names of files to read in
+    lines = na.read_lines(file_path)
+    for i, line in enumerate(lines):
+        if match_string in lines[i-1]:
+            aware_str = na.str_to_local_time(line)
+            if aware_str > latest:
+                return Path(file_path).parent
+
+# update info
+def load_diamon(loc_data, shutter_data):
+    """data from diamon instrument loaded from files. data matched with measurement coordinate
+    Args:
+        loc_data (df): pandas dataframe of x, y, z coordinates and data reference
+        fname (str): folder directory to diamon data
+
+    Returns:
+        dictionary of diamon class objects
+    """
+    diamon_list = [diamon(folder) for folder in glob.glob(diamon_path)]
+    # match the location from the file name ref to coord to get shutter info
+    diamon_list = match_id_location(loc_data, diamon_list, shutter_data)
+    diamon_dict =  {data.file_name: data for data in diamon_list}
+    return diamon_dict
+
+def match_id_location(loc_data, data, shutters):
+    id_list = [diamon_obj.id for diamon_obj in data]
+    id = get_measurement_id(pd.DataFrame(id_list))
+    data = [match_location(loc_data, diamon_obj, id) for diamon_obj in data]
+    return data
+
+def get_measurement_id(df):
+    sorted_df = df.sort_values(by="start").reset_index().drop_duplicates(subset=["start"], keep="first", ignore_index=True)
     sorted_df["count"] = sorted_df.groupby(by=[sorted_df['start'].dt.date, sorted_df["serial"]]).cumcount()+1
     sorted_df["key"] = ((sorted_df["start"].dt.strftime("%d.%m.")) + (sorted_df["count"].astype(str)) + "-" + sorted_df["serial"])
     return sorted_df
 
-#set of functions for string cleaning
-def clean_param(line, uncert=None):
-    """_summary_
+def match_location(location_data, data, id):
+    """match location csv file key to data file name
 
     Args:
-        line (_type_): _description_
-        uncert (_type_, optional): _description_. Defaults to None.
+        location_data (df): pd df of xyz and ref.
+        diamon_list (list): list of diamon class obj
+        series_list (list): list of pd data series containing file id's
 
     Returns:
-        _type_: _description_
+        dic: list of diamon obj with keys assigned
     """
-    line = line.split(":")[1]
-    line = clean(line)
-    if uncert:
-        #uses regular expression package to extract an uncertainty found between a bracket and % symbol
-        line[3] = re.findall('\((.*?)%\)', line[3])
-        return float(line[0]), float(line[3][0])
-    else:
-        return float(line[0])
+    # first check there isnt a duplicate
+    # load location
+    print(data.folder_name)
+    data.file_name = id[id["start"] == data.start_time]["key"].values[0]
 
-def clean_counts(line):
-    line = clean(line)
-    return int(line[1]), int(line[3])
-
-def clean(line):
-    line = line.strip()
-    line = " ".join(line.split())
-    line = line.split()
-    return line
-
-def read_data(folder_path, location_path, shutter_path):
-    
-    shutter_data = na.load_pickle(shutter_path)
-    shutter_data = sa.check_updated_shutter_info(shutter_data)
-    loc_data = na.read_csv(location_path, {'x': float, 'y': float, 'z':float})
-    diamon_data = load_diamon(loc_data, folder_path, shutter_data)
-    return diamon_data
+    #what if cant find reference
+    data.reference = location_data.loc[location_data["Name"] == data.file_name].reset_index()
+    #get beamline and building names for data being measured
+    beamline_df = pd.read_csv("data/target_station_data.csv", index_col=["Building", "Location"])
+    data.beamlines = sa.beamline(data.reference["Measurement Reference"].iloc[0], beamline_df)
+    return data
